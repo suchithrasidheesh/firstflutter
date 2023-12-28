86ba0bae2ce93cf4407245d7940c622d17d1d3be
suchithra <suchi.kp650@gmail.com>
2023-12-28 19:56:48 +0530

    commit

---
+++ lib/List Assignment.dart
@@ -0,0 +1,41 @@
+import 'dart:js';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:firstflutterproject/ListBuilder.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(DevicePreview(
+    isToolbarVisible: true,
+      builder:(BuildContext context)=>MaterialApp(
+           useInheritedMediaQuery: true,
+           debugShowCheckedModeBanner: false,
+           home: ListAssignment(),
+  )));
+}
+class ListAssignment extends StatelessWidget{
+  var food=['Pasta','Maggi','Cake','Pancake','Pizza','Burger','Fries'];
+  var time=['30mins','2mins','45mins','10mins','60mins','35mins','40mins'];
+  var image=['assets/Icons/pasta.png','assets/Icons/maggi.png','assets/Icons/cake.png','assets/Icons/pancake.png','assets/Icons/pizza.png','assets/Icons/burger.png','assets/Icons/fries.png'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text('Recipe Book',),
+      ),
+      body: ListView.builder(itemBuilder: (context,index){
+        return Card(
+          child: ListTile(
+            leading:CircleAvatar(backgroundImage: AssetImage('${image[index]}')),
+            title: Text('${food[index]}'),
+            trailing: Text('${time[index]}'),
+          ),
+
+        );
+      },itemCount:image.length,),
+    );
+  }
+
+}
